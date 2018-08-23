2c96eb72fe29fceee7ffc46fa684c16d1fa1e862
Joel Berger <joel.a.berger@gmail.com>
2018-08-23 18:16:41 -0500

    async/await all the things!

--- ex/pushpin.pl
+++ ex/pushpin.pl
@@ -24,23 +24,23 @@ __DATA__
 @@ pushpin.js
 import * as L from 'https://unpkg.com/leaflet@1.3.4/dist/leaflet-src.esm.js';
 
-var pins = [];
-var map;
+let pins = [];
+let map;
 
 export function initMap() {
   map = L.map('mapid');
   map.doubleClickZoom.disable();
-  var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-  var osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-  var osm = new L.TileLayer(osmUrl, {minZoom: 1, maxZoom: 18, attribution: osmAttrib});
+  let osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  let osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+  let osm = new L.TileLayer(osmUrl, {minZoom: 1, maxZoom: 18, attribution: osmAttrib});
 
   map.setView(new L.LatLng(59.9238, 10.7317), 12);
   map.addLayer(osm);
 
-  map.on('dblclick', function(e) {
-    var text = window.prompt('Would you like to add this pin? ' + e.latlng.toString() + ' Optional text may be added below.');
+  map.on('dblclick', async function(e) {
+    let text = window.prompt('Would you like to add this pin? ' + e.latlng.toString() + ' Optional text may be added below.');
     if (text == null) return;
-    fetch('/pins', {
+    await fetch('/pins', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json; charset=utf-8'},
       body: JSON.stringify({
@@ -48,27 +48,26 @@ export function initMap() {
         lng: e.latlng.lng,
         text,
       }),
-    }).then(() => getPins());
+    });
+    getPins();
   });
 
   getPins();
 }
 
-export function getPins() {
+export async function getPins() {
   removePins();
-  fetch('/pins')
-    .then(res => res.json())
-    .then(data => {
-      var points = [];
-      data.forEach(item => {
-        var point = L.latLng(item.lat, item.lng);
-        points.push(point);
-        var pin = L.marker(point).addTo(map);
-        if (item.text) pin.bindPopup(item.text);
-        pins.push(pin);
-      });
-      map.fitBounds(L.latLngBounds(points), {maxZoom: 12});
-    });
+  let res  = await fetch('/pins');
+  let data = await res.json();
+  let points = [];
+  data.forEach(item => {
+    let point = L.latLng(item.lat, item.lng);
+    points.push(point);
+    let pin = L.marker(point).addTo(map);
+    if (item.text) pin.bindPopup(item.text);
+    pins.push(pin);
+  });
+  map.fitBounds(L.latLngBounds(points), {maxZoom: 12});
 }
 
 export function removePins() {
